49051b6d6642294841ea06ca6273e89eadbb4f6d
jvarghe <vcs.inbox@outlook.com>
2023-01-30 23:57:59 +0000

    Added the 'hello.js' file to the project.

---
+++ hello.js
@@ -0,0 +1,16 @@
+// Example 1: An Anonymous Function:
+const sayHello1  = function () 
+{
+  console.log("Hello, world");
+}
+
+sayHello1();
+
+
+// Example 2: A More Generic Anonymous Function:
+const sayHello2  = function (name) 
+{
+  console.log("Hello, " + name);
+}
+
+sayHello2("Mara");
